df9470fa73e5d51b934ef61367888ca042c63a90
Tasmiya Sarguroh <116432233+Sarguroh20@users.noreply.github.com>
2024-08-22 15:18:36 +0000

    Operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -26,3 +26,41 @@ let someNumber = 50
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// 📌📌📌📌📌📌 Operations 📌📌📌📌📌📌
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// Basic Operations
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " students"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);       // 12
+// console.log(1 + "2");       // 12
+// console.log("1" + 2 + 2);   // 122
+// console.log(1 + 2 + "2");   // 32
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(true);           // true
+// console.log(+true);          // 1
+// console.log(+"");            // 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2  // Output of all: 4
+
+let gameCounter = 100
+++gameCounter;
+// console.log(gameCounter);    // 101
