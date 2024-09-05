d20032f4e3309966675217a67bdf376ab0d69277
Tasmiya Sarguroh <116432233+Sarguroh20@users.noreply.github.com>
2024-09-05 19:10:26 +0000

    Functions with Objects & Array

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -60,3 +60,42 @@ function loginUserMessage(username = "Alice"){
 
 // console.log(loginUserMessage());                 // Alice just logged in 🙂
 console.log(loginUserMessage("TS"));                // Override default values
+
+
+// ... -> Rest & Spread Operator but with different usecase
+// function calculateCartPrice(...num1){            // Here rest operator
+//     return num1
+// }
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))     // [ 200, 400, 500, 2000 ]
+
+// 📝 Interview Q. What values will num1 hold?
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 500, 2000));       // [ 500, 2000 ]
+
+const user = {
+    username: "TS",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)                               // Username is TS and price is 199
+handleObject({
+    username: "Alice",
+    price: 399
+})                                                  // Username is Alice and price is 399
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1] 
+}
+
+// console.log(returnSecondValue(myNewArray));      // 400
+console.log(returnSecondValue([200, 400, 100, 600]));
