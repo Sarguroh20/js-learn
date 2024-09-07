9e447e04e399f14ef39d31d9aef6ccb75b0c472c
Tasmiya Sarguroh <116432233+Sarguroh20@users.noreply.github.com>
2024-09-07 07:07:22 +0000

    Nested Scope

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -7,13 +7,54 @@ if (true) {
     let a = 10
     const b = 20
     var c = 30
-    console.log("Inner: ", a);  
+    // console.log("Inner: ", a);       // Inner:  10
 }
 
-// console.log(a);          // Error: a is not defined (Block Scope: Inside if statement a)
-// console.log(b);          // Error: b is not defined (Block Scope: Inside if statement b)
-// console.log(c);          // 30
+// console.log(a);                      // Error: a is not defined (Block Scope: Inside if statement a)
+// console.log(b);                      // Error: b is not defined (Block Scope: Inside if statement b)
+// console.log(c);                      // 30
 
-console.log("Outer: ", a);             
+// console.log("Outer: ", a);           // Outer:  300       
 
 // 📝 Interview Q. Global Scope is different wrt browser & node code environment. 
+
+
+// Nested Scope
+function one(){
+    const username = "Alice"
+
+    function two(){
+        const website = "google"
+        console.log(username);
+    }
+    // console.log(website);            // Error: website is not defined
+
+    two()
+} 
+
+// one()                                // Alice
+
+if (true) {
+    const username = "Alice"
+    if (username === "Alice") {
+        const website = " youtube"
+        // console.log(username + website);        // Alice youtube
+    }
+    // console.log(website);            // Error: website is not defined
+}
+// console.log(username);               // Error: username is not defined
+
+
+// 📌📌📌📌📌📌 Interesting Facts 📌📌📌📌📌📌
+
+console.log(addone(5));                 // 6
+
+function  addone(num){
+    return num + 1
+}
+
+console.log(addTwo(5))                  // Error: Cannot access 'addTwo' before initialization
+
+const addTwo = function(num){
+    return num + 2
+}
