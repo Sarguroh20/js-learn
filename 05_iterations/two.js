1f0f41927e235db905592daceb359fba0d0e61ab
Tasmiya Sarguroh <116432233+Sarguroh20@users.noreply.github.com>
2024-09-10 18:59:55 +0000

    while & do-while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,18 @@
+let i = 1
+while (i <= 10) {
+    // console.log(`Value of index is ${i}`);
+    i = i + 2;
+}
+
+let myArray = ["Butterscotch", "Pista", "Badam", "Chocolate"]
+let arr = 0;
+while (arr < myArray.length) {
+    // console.log(`Flavor is ${myArray[arr]}`);
+    arr = arr + 1;
+}
+
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
